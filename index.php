a402b42085a04611c1f642d9c1cafa41dcefe9ad
Guilherme Zacarias Beloni <97005507+GuilhermeBeloni@users.noreply.github.com>
2022-04-06 14:28:55 -0300

    .respondividade

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/responsive.css">  
+    
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h2 class="logo">NETFLIX</h2>
+            <nav>
+                <a href="Início">Início</a>
+                <a href="Séries">Séries</a>
+                <a href="Filmes">Filmes</a>
+                <a href="Documentários">Documentários</a>
+            </nav>
+        </div>
+    </header>
+    
+    <main>
+        <div class="filme_principal">
+            <div class="container">
+                <h3 class="titulo">HOUSE OF CARDS</h3>
+                <p class="descricao">Um político inescrupuloso não mede esforços para conquistar o poder nos EUA neste drama vencedor do Emmy e do Globo de Ouro.</p>
+                <div class="botoes">
+                    <button role="button" class="botao">
+                        <i class="fa-solid fa-play"></i>
+                        ASSITIR AGORA
+                    </button>
+                    <button role="button" class="botao">
+                        <i class="fa-solid fa-circle-info"></i>
+                        MAIS INFORMAÇÕES
+                    </button>
+                </div>
+            </div>      
+        </div>
+        <div class="carrossel">
+            <div class="owl-carousel owl-theme">
+                <div class="item"><img class="box-filme" src="img/carousel1.jpg" alt="capa Homem Aranha"></div>
+                <div class="item"><img src="img/carousel2.jpg" alt="capa Cavaleiro da lua"></div>
+                <div class="item"><img src="img/carousel3.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel4.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel5.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel6.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel7.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel8.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel9.jpg" alt=""></div>
+                <div class="item"><img src="img/carousel10.jpg" alt=""></div>
+            </div>
+        </div>
+    </main>
+    
+    <script src="https://kit.fontawesome.com/afaa4f3d45.js" crossorigin="anonymous"></script>
+    <script src="js/owl/jquery.min.js"></script>
+    <script src="js/owl/owl.carousel.min.js"></script>
+    <script src="js/owl/main.js"></script>
+</body>
+</html>
